4ee215d46141bd673fa48cff2824d48ade7ce0ec
Ben Chambule <benchambule@gmail.com>
2019-07-20 19:15:15 +0200

    Updated example to meet the new version of lottus

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from flask import Flask, request, json, Response
 import lottusapp
+import lottus
 
 app = Flask(__name__)
 
 lottus_app = lottusapp.get_lottus_app()
 
-@app.route('/ussdapp/json/', methods=['POST'])
+
+@app.route('/ussdapp/json/', methods=['POST', 'GET'])
 def ussd_json_api():
     js = json.dumps(request.json)
     req_dict = json.loads(js)
     print(req_dict)
 
-    resp = lottus_app.handle_request(req_dict)
+    resp = lottus_app.process_request(req_dict)
+    resp = lottus.create_window_response(resp)
+    print(resp)
 
     return Response(json.dumps(resp), status=200, mimetype='application/json')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- lottusapp.py
+++ lottusapp.py
@@ -1,29 +1,56 @@
-import lottus.core
 import json
 import os
-import lottus.helpers
+import lottus
 
-with open('menus.json') as f:
-    menus = json.load(f)
+with open('windows.json') as f:
+    windows = json.load(f)
 
 def get_lottus_app():
-    lottus_app = lottus.core.Lottus(__name__, 'INITIAL', menus, InMemoryUSSDSessionBag())
+    lottus_app = lottus.Lottus('INITIAL', windows, InMemorySessionManager(), None)
+
+    @lottus_app.window('INITIAL')
+    def process_english_window(session, request):
+        options = [lottus.create_option("1", "English", "ENGLISH"), lottus.create_option("2", "Portuguese", "PORTUGUESE")]
+        window = lottus.create_window("INITIAL", "Ben Chambule's CV", "Select one of following options", options)
+
+        return window, session
 
     return lottus_app
 
 
-class InMemoryUSSDSessionBag(lottus.core.USSDSessionBag):
+def create_window_response(window):
+    """
+    """
+    return {
+        'message': window['message'] if 'message' in window else None,
+        'title': window['title'] if 'title' in window else None,
+        'options': [x for x in create_option_response(window['options'])]
+    } if window else None
+
+def create_option_response(option):
+    """
+    """
+    return {
+        'option': option['option'],
+        'value': option['display']
+    } if option else None
+
+class InMemorySessionManager(lottus.SessionManager):
     def __init__(self):
-        self.__sessions = []
+        self._sessions = []
 
-    def get_session(self, msisdn, session):
-        return next((s for s in self.__sessions if s['session'] == session), None)
+    def get(self, session_nr, cell_nr):
+        return next((s for s in self._sessions if s['session_nr'] == session_nr and s['cell_nr'] == cell_nr), None)
 
-    def update_session(self, session):
-        pass
+    def save(self, session):
+        self._sessions.append(session)
 
-    def save_session(self, session):
-        pass
+# class InMemoryWindowCache(lottus.WindowCache):
+#     def __init__(self):
+#         self.windows = {}
 
-    def add_session(self, session):
-        self.__sessions.append(session)
+#     def get(self, window_name, session_nr=None):
+#         return self.windows[window_name] if window_name in self.windows else None
+
+#     def cache(self, window, session_nr=None):
+#         self.windows[window['name']] = window
